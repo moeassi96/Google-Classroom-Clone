1076880060bb3478bf4f6925953ee30b6064c504
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-22 18:56:21 +0300

    added all sign up steps, and fixed relevant bugs

--- public/src/scripts/router.js
+++ public/src/scripts/router.js
@@ -7,26 +7,26 @@ pages.load_page = (page) => {
     case 'sign-up': {
       let step = 0;
 
-      const content = document.getElementById('sign-in-wrapper');
-      //   signUpForm(step, content);
+      const content = document.getElementById('form-body');
+
       content.innerHTML = signUpForm(step);
-      const next = document.getElementById('sign-up-next');
+      const next = document.getElementById('next-button');
       const first_name = document.getElementById('first-name');
       const last_name = document.getElementById('last-name');
 
       next.addEventListener('click', () => {
-        console.log('clicked');
-        if (step === 0) {
-          if (first_name.value === '') {
-            first_name.nextElementSibling.innerHTML =
-              'First name cannot be empty';
-            return;
-          } else if (last_name.value === '') {
-            last_name.nextElementSibling.innerHTML =
-              'Last name cannot be empty';
-            return;
-          }
-        }
+        console.log(step);
+        // if (step === 0) {
+        //   if (first_name.value === '') {
+        //     first_name.nextElementSibling.innerHTML =
+        //       'First name cannot be empty';
+        //     return;
+        //   } else if (last_name.value === '') {
+        //     last_name.nextElementSibling.innerHTML =
+        //       'Last name cannot be empty';
+        //     return;
+        //   }
+        // }
         step++;
         content.innerHTML = signUpForm(step);
       });
