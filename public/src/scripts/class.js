d69c529ec3d0b1646fb1954b133a97a66e181dbf
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-24 23:32:30 +0300

    <FE> added displayClassDetails dynamically

--- public/src/scripts/class.js
+++ public/src/scripts/class.js
@@ -1,21 +1,20 @@
 function displaystream(assignments) {
+	const assignmentsList = document.getElementById("streamFeed");
+	assignmentsList.innerHTML = "";
 
-  const assignmentsList = document.getElementById('streamFeed');
-  assignmentsList.innerHTML = '';
-
-  //Looping over assignments, then appending to assignments-container
-  assignments.forEach((assignment, index) => {
-    assign_id = assignment.assignment_id;
-    creator =
-      assignment.user_firstname +
-      ' ' +
-      assignment.user_lastname +
-      ' posted a new assignment:';
-    title = creator + assignment.assignment_name;
-    date = assignment.assignment_date;
-    description = assignment.assignment_description;
-    (attachmentCaption = 'Awesome'),
-      (assignmentsList.innerHTML += `<div class="stream-content-box cursor-pointer">
+	//Looping over assignments, then appending to assignments-container
+	assignments.forEach((assignment, index) => {
+		assign_id = assignment.assignment_id;
+		creator =
+			assignment.user_firstname +
+			" " +
+			assignment.user_lastname +
+			" posted a new assignment:";
+		title = creator + assignment.assignment_name;
+		date = assignment.assignment_date;
+		description = assignment.assignment_description;
+		(attachmentCaption = "Awesome"),
+			(assignmentsList.innerHTML += `<div class="stream-content-box cursor-pointer">
     <a href='assignment.html?assignment_id=${assign_id}'>
         <div class="stream-assignment-box flex">
             <div class="stream-assignment-icon flex items-center justify-center">
@@ -32,57 +31,90 @@ function displaystream(assignments) {
         </div>
     </a>
 </div>`);
-  });
+	});
 }
 
-window.addEventListener('load', async () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const class_id = urlParams.get('class_id');
-  const user_id = localStorage.getItem('user_id');
-  sideBar({})
-  const user = {
-    user_id,
-    class_id,
-  };
+async function getClassDetails() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const class_id = urlParams.get("class_id");
 
-  // getting role of user in this class
-  const roleresponse = await fetch(
-    'http://localhost/google-clone/Google-Classroom-Clone/api/controllers/role.php',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    }
-  );
+	const response = await fetch(
+		"http://localhost/fsw/Google-Classroom-Clone/api/controllers/getSingleClass.php",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ class_id }),
+		}
+	);
 
-  const role = await roleresponse.json();
-  
+	const result = await response.json();
+	classDetails = result[0];
 
-  classworkbtn = document.getElementById('classwork');
-  peopleBtn = document.getElementById('people');
+  displayClassDetails(classDetails)
+}
 
-  classworkbtn.addEventListener('click', () => {
-    window.location.href = `classwork.html?class_id=${class_id}`;
-  });
+function displayClassDetails(classDetails) {
+	const navbar_class_title = document.getElementById("navbar-class-title");
+	const hero_class_title = document.getElementById("hero-class-title");
+	const class_google_meet_link = document.getElementById("class-google-meet-link");
 
-  peopleBtn.addEventListener('click', () => {
-    window.location.href = `people.html?class_id=${class_id}`;
-  });
+  navbar_class_title.innerHTML = `<p>${classDetails.class_name}<br><span class="section">${classDetails.class_subject}</span></p>`
+  hero_class_title.innerHTML = `${classDetails.class_name}<br /> <span class="stream-class-section">${classDetails.class_subject}</span>`
+  class_google_meet_link.href = classDetails.meeting_link
 
-  const response = await fetch(
-    'http://localhost/google-clone/Google-Classroom-Clone/api/controllers/getassignments.php',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ class_id }),
-    }
-  );
+}
 
-  const assignments = await response.json();
+window.addEventListener("load", async () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	const class_id = urlParams.get("class_id");
+	const user_id = localStorage.getItem("user_id");
+	sideBar({});
+	getClassDetails();
 
-  displaystream(assignments);
+	const user = {
+		user_id,
+		class_id,
+	};
+
+	// getting role of user in this class
+	const roleresponse = await fetch(
+		"http://localhost/fsw/Google-Classroom-Clone/api/controllers/role.php",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(user),
+		}
+	);
+
+	const role = await roleresponse.json();
+
+	classworkbtn = document.getElementById("classwork");
+	peopleBtn = document.getElementById("people");
+
+	classworkbtn.addEventListener("click", () => {
+		window.location.href = `classwork.html?class_id=${class_id}`;
+	});
+
+	peopleBtn.addEventListener("click", () => {
+		window.location.href = `people.html?class_id=${class_id}`;
+	});
+
+	const response = await fetch(
+		"http://localhost/fsw/Google-Classroom-Clone/api/controllers/getassignments.php",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ class_id }),
+		}
+	);
+
+	const assignments = await response.json();
+
+	displaystream(assignments);
 });
