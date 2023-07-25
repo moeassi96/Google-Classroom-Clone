b72cc6c71db17872e762646bedde68ebaad976e6
moeassi96 <117896769+moeassi96@users.noreply.github.com>
2023-07-25 12:12:00 +0300

    <FE><BE> Added invite button for teachers in people page

--- public/src/scripts/people.js
+++ public/src/scripts/people.js
@@ -1,7 +1,31 @@
 window.addEventListener("load", async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const class_id = urlParams.get("class_id");
+  const user_id = localStorage.getItem("user_id")
+
+
   sideBar({});
+  
+  const user = {
+    class_id,
+    user_id
+  }
+  const user_role = await fetch(
+    "http://localhost/google-clone/Google-Classroom-Clone/api/controllers/role.php",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  );
+  const role = await user_role.json();
+  
+  if(role.role === "teacher" || role.role === "Teacher"){
+    document.getElementById("invite").style.display = "block";
+  }
+  
 
   const res = await fetch(
     "http://localhost/google-clone/Google-Classroom-Clone/api/controllers/getteachers.php",
@@ -38,4 +62,13 @@ window.addEventListener("load", async () => {
   classworkbtn.addEventListener("click", () => {
     window.location.href = `classwork.html?class_id=${class_id}`;
   });
+
+
+  const inviteBtn = document.getElementById("invite");
+
+  inviteBtn.addEventListener("click", () => {
+    window.location.href = `class.html?class_id=${class_id}`;
+  });
+
+
 });
