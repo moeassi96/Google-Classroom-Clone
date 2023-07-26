0f7b2d606cb2ebed84236091167cced394077df6
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-26 03:54:25 +0300

    <BE> added get teacher classes api

---
+++ api/controllers/myClassesTeacher.php
@@ -0,0 +1,22 @@
+<?php
+include('connection.php');
+
+header("Content-Type: application/json");
+$data = json_decode(file_get_contents('php://input'), true);
+
+$user_id = $data["user_id"];
+
+$query = $mysqli->prepare(
+    "SELECT classes.*, `classes-enrollments`.role
+    FROM classes
+    JOIN `classes-enrollments` ON classes.class_id = `classes-enrollments`.class_id
+    WHERE `classes-enrollments`.user_id = ? AND `classes-enrollments`.role = 'teacher'"
+);
+
+$query->bind_param('s', $user_id);
+$query->execute();
+$result = $query->get_result();
+$classes = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($classes);
+?>
