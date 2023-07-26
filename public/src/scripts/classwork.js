fa3c18861161649de1411720513835d150b6c8af
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-26 03:17:06 +0300

    <FE> added role based showing of the assign button

--- public/src/scripts/classwork.js
+++ public/src/scripts/classwork.js
@@ -1,5 +1,3 @@
-const role = "teacher";
-
 function displayAssignment(assignments) {
   const assignmentsList = document.getElementById('assignments-container');
   assignmentsList.innerHTML = '';
@@ -134,16 +132,36 @@ window.addEventListener('load', async () => {
   });
 });
 
-function modifyPersonalLinkDiv(){
-  console.log("modify")
+async function modifyPersonalLinkDiv(){
+  const urlParams = new URLSearchParams(window.location.search);
+  const class_id = urlParams.get('class_id');
+  const user_id = localStorage.getItem("user_id")
+  
+  const user = {
+    class_id,
+    user_id
+  }
+  const user_role = await fetch(
+    "http://localhost/google-clone/Google-Classroom-Clone/api/controllers/role.php",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  );
+  const role = await user_role.json();
+  console.log(role.role)
+
   const PL = document.getElementById("personal-link");
-  if (role == "teacher"){
+  if (role.role == "teacher"){
     PL.innerHTML = '<div class="add-assignment flex justify-start gap-9" id="add-assignment"><label for="add-assignment-btn" class="add-assignment-btn-label" id="add-assignment-btn-label">Assign</label><button class="add-assignment-btn" id="add-assignment-btn"></button> </div>'
     const urlParams = new URLSearchParams(window.location.search);
     const class_id = urlParams.get('class_id');
     const addAssign = document.getElementById("add-assignment-btn-label");
     addAssign.addEventListener('click',() => {
-      window.location.href = `createassign.html?class_id=${class_id}`;
+      window.location.href = `createAssignment.html?class_id=${class_id}`;
     })
   }
   else{
